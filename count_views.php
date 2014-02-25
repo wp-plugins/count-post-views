d66972e5c4bd69ca51848664572d6f145c2fd152
tuanva <tuanva@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-25 18:54:48 +0000

    Update version
    git-svn-id: https://plugins.svn.wordpress.org/count-post-views/trunk@864885 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- count_views.php
+++ count_views.php
@@ -3,15 +3,12 @@
 Plugin Name: Count Post Views
 Author: TuanVA
 Author URI: http://tamhuyet.com
-Plugin URI: http://tamhuyet.com/wpplugins/
-Version: 1.0
+Plugin URI: http://tamhuyet.com/wplugins/
+Version: 1.0.1
 Description: Count views if your Posts in wordpress. <br>Simple to emble to with simple code <strong>echo tamhuyet_count();</strong> or <strong>echo tamhuyet_count($post_id);</strong> with $post_id is id of post or page.
 */
 
 global $wp_version;
-$tb_name = "count_views_db";
-$table_name = $wpdb->prefix .$tb_name;
-
 if(version_compare($wp_version,'3.0.3','<')){
 	exit("This plugin's support wp version 3.0.3 or higher. You need upgrade your wordpress version to use this plugin!");
 }
@@ -19,7 +16,8 @@ if(version_compare($wp_version,'3.0.3','<')){
 function tamhuyet_create_db()
 {
 	global $wpdb;
-	global $table_name;
+	$tb_name = "count_views_db";
+	$table_name = $wpdb->prefix.$tb_name;
     $sql = "CREATE TABLE $table_name (
       id int(11) NOT NULL AUTO_INCREMENT,
       item int(11) DEFAULT NULL,
@@ -34,7 +32,9 @@ function tamhuyet_create_db()
 function tamhuyet_check_id($id)
 {
 	global $wpdb;
-	global $table_name;
+	global $wpdb;
+	$tb_name = "count_views_db";
+	$table_name = $wpdb->prefix.$tb_name;
     $row = $wpdb->get_row($wpdb->prepare('SELECT id FROM '.$table_name.' WHERE item = %d',$id) );
     if($row->id != "")
     	return true;
@@ -43,7 +43,9 @@ function tamhuyet_check_id($id)
 
 function tamhuyet_insert_qb($id){
 	global $wpdb;
-	global $table_name;
+	global $wpdb;
+	$tb_name = "count_views_db";
+	$table_name = $wpdb->prefix.$tb_name;
 
 	if(tamhuyet_check_id($id)){
 		$views = $wpdb->get_row($wpdb->prepare('SELECT value FROM '.$table_name.' WHERE item = '.$id.''));
@@ -67,7 +69,9 @@ function tamhuyet_insert_qb($id){
 
 function tamhuyet_count(){
 	global $wpdb;
-	global $table_name;
+	global $wpdb;
+	$tb_name = "count_views_db";
+	$table_name = $wpdb->prefix.$tb_name;
 	global $wp_query; $postid = $wp_query->post->ID;
 	tamhuyet_insert_qb($postid);
 
